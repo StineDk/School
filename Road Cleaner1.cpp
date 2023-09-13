18d992810cc10c2aaf63a648b471eb7894b8c1b9
StineDk <144903306+StineDk@users.noreply.github.com>
2023-09-13 13:58:30 +0200

    Add files via upload

--- Road Cleaner1.cpp
+++ Road Cleaner1.cpp
@@ -5,23 +5,39 @@
 using namespace std;
 int main()
 {
-    int StartPris, Skrab, Meter;
-    StartPris = 750;
+    float StartPris, Skrab;
+    StartPris = 750.;
     float Klip = 50. / 100.;
-    Skrab = 100 / 100;
-    Meter;
+    Skrab = 100. / 100.;
+    float Meter; 
+    float Klipning;
+    float Skrabning;
     
-    // Kunden skal have klippet 1500 meter
-    cout << Klip * 1500 + StartPris << endl;
-    // Kunden skla have skrabbet 1500 meter
-    cout << Skrab * 1500 + StartPris << endl;
+    // Kunden skal have klippet 1500 meter;
+    //cout << Klip * 1500 + StartPris;
+    // Kunden skla have skrabbet 1500 meter;
+    //cout << Skrab * 1500 + StartPris;
 
     string Type;
-    // Vælg mellem Klip eller Skrab
-    cout << "Vælg Klip eller Skrab";
-    cin >> Type; 
+    // Vælg mellem Klip eller Skrab;
+    cout << "Vealg Klip eller Skrab";
+    cin >> Type;
 
-    {
-        
+    if (Type == "Klip") {
+        cout << "skriv antal meter (1 = 1m)";
+        cin >> Meter;
+
+        Klipning = (Klip * Meter) + StartPris;
+
+        cout << "Klipning " << Klipning << ",- DKK \n";
+    }
+    // else if;
+    if (Type == "Skrab") {
+        cout << "Skriv antal meter (1 = 1m)";
+        cin >> Meter;
+
+        Skrabning = (Skrab * Meter) + StartPris;
+
+        cout << "Skrabning " << Skrabning << ",- DKK \n";
     }
 }
